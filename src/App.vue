397dcdfa1fd29934d7a13e9622766c1185882bd1
ramon alex <ramon.alex@neppoti.com.br>
2019-10-08 19:21:19 -0300

    continua errado

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" >
+  <div id="app">
     <NavBar msg="LembreSee"/>
     <div>
 
@@ -21,30 +21,29 @@ export default {
   },
   data(){
     return{
+      item: String, 
       images:[
-        {img: '"./assets/img1.jpg"'},
-        {img: '"./assets/img2.jpg"'},
-        {img: '"./assets/img3.jpg"'},
-        {img: '"./assets/img4.jpg"'},
-        {img: '"./assets/img5.jpg"'},
-        {img: '"./assets/img6.jpg"'},
+        "./assets/img1.jpg",
+        "./assets/img2.jpg",
+        "./assets/img3.jpg",
+        "./assets/img4.jpg",
+        "./assets/img5.jpg",
+        "./assets/img6.jpg",
       ]
     }
   },
   methods:{
     changeBackgroundImage() {
-      // console.log(this.images)
-      // console.log( Math.floor(Math.random() * this.images.length))
+      this.item = this.images[Math.floor(Math.random() * this.images.length)]
+      console.log(this.item)
+      // document.body.style.backgroundImage = "url(" + this.item + ")"
 
-      var item = this.images[Math.floor(Math.random() * this.images.length)].img
-      console.log(item)
-      // document.body.style.backgroundImage = "url(" + item + ")"
-      document.body.style.backgroundImage = "url('./assets/img6.jpg')";
+      document.body.style.backgroundImage = `url(${require(this.item)})`;
+      // document.body.style.backgroundImage = "url(" + ${require(item)} + ")";
+      // document.body.style.backgroundImage = "url('./assets/img6.jpg')";
+      // document.body.style.backgroundImage = `url(${require('./assets/img1.jpg')})`;
+      document.body.style.backgroundSize  =  '100% 100%'
       // document.body.style.background= 'red'
-
-
-
-      // console.log(document.body.style.backgroundImage = "url(" + this.images[i].img + ")")
     },
   },
   created: function(){
